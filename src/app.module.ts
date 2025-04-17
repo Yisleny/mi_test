da88e8b42efc59db63a7ef112e56989ed5fbcbe8
Yislenys <yislenys86@gmail.com>
2025-04-17 17:43:42 -0400

    Segundo deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestController } from './test.controller';
+import { TestService } from './test.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TestController],
-  providers: [AppService],
+  providers: [AppService, TestService],
 })
 export class AppModule {}
